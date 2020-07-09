3c2a765d9ee7747b4c99c8b6c17024b43d744114
B1ackin <xx13.02@gmail.com>
2020-07-09 14:31:46 +0300

    Updates

---
+++ precache-manifest.e53a4ecd9cd21ff065bd035f1fdbbb72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60738613d27dd9b7b7536668072b6f16",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "cadbe5f7aa423019aced",
+    "url": "/social-network/static/css/main.d106968a.chunk.css"
+  },
+  {
+    "revision": "bc2ef206bcf7d4721cbb",
+    "url": "/social-network/static/js/2.431b0eea.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-network/static/js/2.431b0eea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cadbe5f7aa423019aced",
+    "url": "/social-network/static/js/main.5af6e61b.chunk.js"
+  },
+  {
+    "revision": "68b01136d249e5809ba9",
+    "url": "/social-network/static/js/runtime-main.bf681233.js"
+  },
+  {
+    "revision": "5c924273775d1c31074cca1ddc3e28f8",
+    "url": "/social-network/static/media/obl.5c924273.jpg"
+  }
+]);
