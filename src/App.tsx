c09b0c68f5d22e4641b6f144e65c384070b16dbe
B1ackin <xx13.02@gmail.com>
2020-07-09 13:54:40 +0300

    dev

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ function App(props: PostsType) {
                 <div className="app-wrapper">
                     <Header/>
                     <Navbar/>
-                    <h1>тест коммит</h1>
+                    <h1>тест коммит</h1>Ты в ветке dev
                     <div className='app-wrapper-content'>
                         <Route path="/profile" render={() => <Profile profilePage={props.state.profilePage} addPost={props.addPost}/>}/>
                         <Route path="/dialogs"
