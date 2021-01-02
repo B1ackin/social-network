9172d5d24c6e6d2232a66f524fa63ebc140ce64e
B1ackin <xx13.02@gmail.com>
2021-01-02 17:32:27 +0300

    lesson 75 done

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,7 +1,45 @@
 import React from "react";
+import {reduxForm, Field, InjectedFormProps} from "redux-form";
+
+export type FormDataType = {
+    login: string
+    password: string
+    rememberMe: boolean
+}
+
+
+const LoginForm: React.FC<InjectedFormProps<FormDataType>> = (props) => {
+    return <div>
+    <form onSubmit={props.handleSubmit}>
+        <div>
+            <Field placeholder={"Login"} name={"login"} component={"input"}/>
+        </div>
+        <div>
+            <Field placeholder={"Password"} name={"password"} component={"input"}/>
+        </div>
+        <div>
+            <Field type={"checkbox"} name={"rememberMe"}  component={"input"}/> remember me
+        </div>
+        <div>
+            <button>Login</button>
+        </div>
+    </form>
+    </div>
+}
+
+const LoginReduxForm = reduxForm<FormDataType>({
+    form: 'login'
+})(LoginForm)
 
 const Login = () => {
-    return <h1>LOGIN</h1>
+    const onSubmit = (formData:FormDataType) => {
+
+    }
+
+    return <div>
+        <h1>LOGIN</h1>
+        <LoginReduxForm onSubmit={onSubmit}/>
+    </div>
 }
 
 export default Login;
--- src/redux/redux-store.tsx
+++ src/redux/redux-store.tsx
@@ -5,26 +5,17 @@ import sidebarReducer from "./sidebar-reducer";
 import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
 import thunkMiddleware from "redux-thunk"
+import {reducer as formReducer } from 'redux-form'
 
 let reducers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogReducer,
     sidebar: sidebarReducer,
     usersPage: usersReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 });
 
-// reducer {
-//     prP: {
-//
-//     },
-//     diP: [
-//         {},
-//         {},
-//         {},
-//         {},
-//     ]
-// }
 
 let store:any = createStore(reducers, applyMiddleware(thunkMiddleware));
 
